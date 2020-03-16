4b5a2f88be0df025ebfc6945779934626f5f8373
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-16 18:58:18 +0100

    :recycle: Refactor lifecycled-component to ease test

--- src/examples.cljs
+++ src/examples.cljs
@@ -8,11 +8,16 @@
     [:span (str value)]
     [:button {:on-click #(set-attribute :value (- value 1))} "-"]]])
 
-(comment (defcomponent lifecycled-component
-           {:on-enter (fn [state] (println "Enter"))
-            :on-update (fn [state] (println "Update"))
-            :on-exit (fn [state] (println "Out"))
-            :props ["value"]
-            :render (fn [state] [:div.class "Render"])
-            :hook (fn [state] (swap! state
-                                     #(assoc % :value (+ (:value %) 1))))}))
+(defcomponent lifecycled-component
+  {:on-enter (fn [this] (println "Enter"))
+   :on-update (fn [this] (println "Update"))
+   :on-exit (fn [this] (println "Out"))
+   :props ["value"]
+   :render (fn [{:keys [value]}]
+             [:div "Hello world! This is an awesome counter!"
+              [:div
+               [:button {:on-click #(set-attribute :value (+ value 1))} "+"]
+               [:span (str value)]
+               [:button {:on-click #(set-attribute :value (- value 1))} "-"]]])
+   :hook (fn [state] (swap! state
+                            #(assoc % :value (+ (:value %) 1))))})
