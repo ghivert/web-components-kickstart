936f40cd89d4156f639d7fe1f4f412b210e09520
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-20 10:18:47 +0100

    :sparkles: Correctly concat children with fragments

--- src/hiccup/core.cljs
+++ src/hiccup/core.cljs
@@ -1,10 +1,20 @@
 (ns hiccup.core)
 
+(defn- concat-children
+  ([children] (concat-children children []))
+  ([[child & children] acc]
+   (if (some? child)
+     (if (= :<> (first child))
+       (concat-children (concat (rest child) children) acc)
+       (concat-children children (conj acc child)))
+     acc)))
+
 (defn reconcile [[keyword props & children]]
-  (let [is-map? (map? props)]
+  (let [is-map? (map? props)
+        all-children (if is-map? children (cons props children))]
     [(name keyword)
      (if is-map? props {})
-     (if is-map? children (cons props children))]))
+     (concat-children all-children)]))
 
 (defn tag-name [[key]]
   (name key))
@@ -13,4 +23,5 @@
   (if (map? p) p {}))
 
 (defn children [[_ p & children]]
-  (if (map? p) children (cons p children)))
+  (let [all-children (if (map? p) children (cons p children))]
+    (concat-children all-children)))
