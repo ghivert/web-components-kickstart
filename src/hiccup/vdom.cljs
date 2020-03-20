fe6d4f576bc76284926a2341b55909fde8762104
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-20 11:03:57 +0100

    :sparkles: Correctly handle VDOM

--- src/hiccup/vdom.cljs
+++ src/hiccup/vdom.cljs
@@ -2,6 +2,8 @@
   (:require [clojure.string :refer [lower-case starts-with?]]
             [hiccup.core :as hiccup]))
 
+(declare diff patch)
+
 (defn isolate-props-events [props]
   (split-with (fn [param]
                 (starts-with? (name (first param)) "on"))
@@ -16,26 +18,42 @@
         [to-add to-update to-delete] (intersect-props node props)]
     node))
 
-(defn diff-children [node children])
+(defn remove-remaining-children [children]
+  (doseq [child children]
+    (.removeChild (.-parentNode child) child)))
 
-(defn create-new-node [name props children]
-  (doto (js/document.createElement name)
-        (update-node-props props)
-        (diff-children children)))
+(defn diff-children
+  ([node children] (diff-children node (array-seq (.-childNodes node)) children))
+  ([node [first-existing & rest-existing] [first-child & rest-child]]
+   (if (some? first-child)
+     (do
+       (if (some? first-existing)
+         (diff first-existing first-child)
+         (.appendChild node (patch nil first-child)))
+       (diff-children node rest-existing rest-child))
+     (remove-remaining-children (concat first-existing rest-existing)))))
+
+(defn select-node [node name]
+  (if (nil? node)
+    (js/document.createElement name)
+    node))
 
 (defn patch [node vec-dom]
-  (let [[name props children] (hiccup/reconcile vec-dom)]
-    (if (nil? node)
-      (create-new-node name props children)
-      (doto node
+  (if (string? vec-dom)
+    (js/document.createTextNode vec-dom)
+    (let [[name props children] (hiccup/reconcile vec-dom)
+          to-patch (select-node node name)]
+      (doto to-patch
             (update-node-props props)
             (diff-children children)))))
 
 (defn diff [root vec-dom]
-  (println vec-dom)
-  (js/console.log root)
-  (let [el-name (hiccup/tag-name vec-dom)
-        tag-name (lower-case (.-tagName root))]
-    (if (= el-name tag-name)
-      (patch root vec-dom)
-      (.replaceChild (.-parentNode root) (patch nil vec-dom) root))))
+  (let [text-root? (= (.-nodeType root) (.-TEXT_NODE js/Node))]
+    (if (and text-root? (string? vec-dom))
+      (when-not (= (.-nodeValue root) vec-dom)
+        (set! (.-nodeValue root) vec-dom))
+      (let [el-name (hiccup/tag-name vec-dom)
+            tag-name (lower-case (or (.-tagName root) ""))]
+        (if (= el-name tag-name)
+          (patch root vec-dom)
+          (.replaceChild (.-parentNode root) (patch nil vec-dom) root))))))
