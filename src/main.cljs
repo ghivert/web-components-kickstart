e2b77a6e09a7f6c01bb8d67ac5539de6efa7d50a
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-20 13:01:12 +0100

    :wrench: Main for soft reload

--- src/main.cljs
+++ src/main.cljs
@@ -2,10 +2,16 @@
   (:require [web-component.core :as wc :refer-macros [defcomponent]]
             [examples]))
 
+(def app-state (atom {:user nil}))
+
+(def ui)
+
 (defn main! []
   (println "Main")
   (let [app (js/document.getElementById "app")]
-    (wc/render! app (examples/app-root))))
+    (wc/render! app (examples/awesome-counter {:value 0}))))
 
 (defn reload! []
-  (println "Reload"))
+  (println "Reload")
+  (let [node (js/document.querySelector "#app > awesome-counter")]
+    (.setAttribute node "value" (+ 1 (js/parseInt (.-value node))))))
